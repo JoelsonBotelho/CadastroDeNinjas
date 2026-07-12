300d254740e5f7ad9c2505d7f80a39144ba44ee2
Joelson Botelho <107374263+JoelsonBotelho@users.noreply.github.com>
2026-07-12 18:46:41 +0100

    Alterei de Controller para NinjaController

--- src/main/java/dev/jav10x/CadastroDeNinjas/NinjaController.java
+++ src/main/java/dev/jav10x/CadastroDeNinjas/NinjaController.java
@@ -3,12 +3,11 @@ package dev.jav10x.CadastroDeNinjas;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 @RestController
 @RequestMapping
-public class Controller {
+public class NinjaController {
     @GetMapping("/boasvindas")
     public String boasVindas() {
         return "Essa é a minha primeira mensagem nessa rota";
