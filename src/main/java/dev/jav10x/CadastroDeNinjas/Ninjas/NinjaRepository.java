58b3c1a6c502c0c3fdad801ad929a4bda1ed79ea
Joelson Botelho <107374263+JoelsonBotelho@users.noreply.github.com>
2026-07-13 17:07:17 +0100

    feat: Add o Ninja Repository e conecta ao JPA

--- src/main/java/dev/jav10x/CadastroDeNinjas/Ninjas/NinjaRepository.java
+++ src/main/java/dev/jav10x/CadastroDeNinjas/Ninjas/NinjaRepository.java
@@ -1,5 +1,7 @@
 package dev.jav10x.CadastroDeNinjas.Ninjas;
 
-public class NinjaRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NinjaRepository extends JpaRepository <NinjaModel, Long>{
     
 }
