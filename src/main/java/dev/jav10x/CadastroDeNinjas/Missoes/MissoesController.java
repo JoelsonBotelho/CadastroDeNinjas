960dcb189a6b7c5d4fa3e8187904a976b1612fb5
Joelson Botelho <107374263+JoelsonBotelho@users.noreply.github.com>
2026-07-15 19:23:39 +0100

    feat: Criar as do controller com mock

--- src/main/java/dev/jav10x/CadastroDeNinjas/Missoes/MissoesController.java
+++ src/main/java/dev/jav10x/CadastroDeNinjas/Missoes/MissoesController.java
@@ -1,5 +1,37 @@
 package dev.jav10x.CadastroDeNinjas.Missoes;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/missoes")
 public class MissoesController {
-    
+    @PostMapping("/criar")
+    public String criarMissao(){
+        return "Missão criada com sucesso";
+    }
+
+    @GetMapping("/listar")
+    public String listarMissao(){
+        return "Listando Missões";
+    }
+
+    @GetMapping("/listarID")
+    public String listarMissaoPorID(){
+        return "Listar Missão por ID"; 
+    }
+
+    @PutMapping("/alterarID")
+    public String alterarMissaoPorID(){
+        return "Alterar Missão por ID";
+    }
+
+    @DeleteMapping("/deletarID")
+    public String deletarMissaoPorID(){
+        return "Deletar Missão por ID";
+    }
 }
