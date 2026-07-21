719f2927eedc80334047e90deb415276b2179c4d
Joelson Botelho <107374263+JoelsonBotelho@users.noreply.github.com>
2026-07-21 11:21:13 +0100

    fix: Exclui o atributo minja

--- src/main/java/dev/jav10x/CadastroDeNinjas/Missoes/MissoesDTO.java
+++ src/main/java/dev/jav10x/CadastroDeNinjas/Missoes/MissoesDTO.java
@@ -1,8 +1,5 @@
 package dev.jav10x.CadastroDeNinjas.Missoes;
 
-import java.util.List;
-
-import dev.jav10x.CadastroDeNinjas.Ninjas.NinjaModel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,6 +12,5 @@ public class MissoesDTO {
     private Long id;
     private String nome;
     private String dificuldade;
-    private List<NinjaModel> ninjas;
 
 }
